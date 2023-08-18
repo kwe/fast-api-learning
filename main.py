24084aae2a1b7dee17de444d0b8c808716623d95
kwe <kwevans@gmail.com>
2023-08-18 12:42:37 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from typing import List
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Reference(BaseModel):
+  source: str
+  source_text: str
+  filename: str
+  page: str
+class Response(BaseModel):
+    answer: str
+    time: str
+    references: List[Reference]
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+  return {"Hello": "World"}
+
+@app.post("/prompt")
+def read_prompt(prompt: str) -> Response:
+  references = [
+    Reference(
+      source="Wikipedia",
+      source_text="The number of the French department of Maine-et-Loire is 49.",
+      filename="wikipedia.txt",
+      page="https://en.wikipedia.org/wiki/Maine-et-Loire"
+    ),
+    Reference(
+      source="Wikipedia",
+      source_text="The number of the French department of Maine-et-Loire is 49.",
+      filename="wikipedia.txt",
+      page="https://en.wikipedia.org/wiki/Maine-et-Loire"
+    ),
+  ]
+  return Response(answer="49",
+          time="27 seconds",
+          references=references,
+  )
